e92aa538bff2b6bf380de50a2a20eba13a729f11
jason <335326056@qq.com>
2018-07-30 19:25:39 +0800

    add pandas test demo from office

--- pandas_test.py
+++ pandas_test.py
@@ -3,9 +3,21 @@ import pandas as pd
 import requests
 
 
-PATH = r'D:\\Eclipse_worksplace\\pandasPro\\data'
+PATH = r'D:\\Eclipse_worksplace\\pandasPro\\data\\'
 
 r = requests.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data')
 
+with open(PATH + 'iris.data','w') as f:
+    f.write(r.text)
+   
+retval = os.getcwd()
+print('current dir:%s'%retval)
+os.chdir(PATH)
 
+df = pd.read_csv(PATH+'iris.data',names=['sepal lenth','sepal width','petal length','pettal with','class'])
+
+print(df.head())
+
+retval = os.getcwd()
+print('current dir:%s' %retval)
 print('test')
