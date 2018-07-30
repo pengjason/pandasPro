794162e8e73d8ed637dbb7b3a5608efc9ab83154
jason <335326056@qq.com>
2018-07-30 18:18:58 +0800

    create new pandas pro from office

---
+++ pandas_test.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+import requests
+
+
+PATH = r'D:\\Eclipse_worksplace\\pandasPro\\data'
+
+r = requests.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data')
+
+
+print('test')
