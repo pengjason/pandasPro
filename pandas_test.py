db36e00c5e14c553f69ee751c88e0ef02eb1fedb
jason <335326056@qq.com>
2018-07-31 18:20:30 +0800

    add pandas slince demo from office

--- pandas_test.py
+++ pandas_test.py
@@ -5,19 +5,25 @@ import requests
 
 PATH = r'D:\\Eclipse_worksplace\\pandasPro\\data\\'
 
-r = requests.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data')
-
-with open(PATH + 'iris.data','w') as f:
-    f.write(r.text)
-   
+# r = requests.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data')
+# 
+# with open(PATH + 'iris.data','w') as f:
+#     f.write(r.text)
+#    
 retval = os.getcwd()
 print('current dir:%s'%retval)
 os.chdir(PATH)
 
-df = pd.read_csv(PATH+'iris.data',names=['sepal lenth','sepal width','petal length','pettal with','class'])
+df = pd.read_csv(PATH+'iris.data',names=['sepal length','sepal width','petal length','pettal width','class'])
 
 print(df.head())
 
+print(df['sepal length'])
+print(df.ix[:3,:2])
+print(df.ix[:3,[x for x in df.columns if 'width' in x]])
+print(df['class'].unique())
+
+print(df[df['class']=='Iris-virginica'])
 retval = os.getcwd()
 print('current dir:%s' %retval)
 print('test')
